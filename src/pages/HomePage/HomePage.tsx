b2e7d41359f6bcf6a3854ae245daa175c1726eff
Sterling Watts <sterling.watts@mindera.com>
2021-09-02 23:56:51 -0700

    Add HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,10 +6,15 @@ import "./HomePage.css";
 const HomePage: React.FC = () => {
   return (
     <Page className="HomePage" color="blue">
-      <Button type="contained" color="yellow" label="JOIN" />
-      <Button type="contained" color="yellow" label="START" />
-      <Button type="text" color="pink" label="ADD STORIES" />
-      <Button type="text" color="pink" label="EDIT STORIES" />
+      <header>
+        <h1>MINDERA POKER</h1>
+      </header>
+      <div className="buttons">
+        <Button type="contained" color="yellow" label="JOIN" />
+        <Button type="contained" color="yellow" label="START" />
+        <Button type="text" color="pink" label="ADD STORIES" />
+        <Button type="text" color="pink" label="EDIT STORIES" />
+      </div>
     </Page>
   );
 };
