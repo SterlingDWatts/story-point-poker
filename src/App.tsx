4b1387a070c8dda5c895d52a4da91799745ad2fe
Sterling Watts <sterling.watts@mindera.com>
2021-09-05 23:05:20 -0700

    Add SelectPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router";
 import HomePage from "./pages/HomePage/HomePage";
 import JoinPage from "./pages/JoinPage/JoinPage";
+import SelectPage from "./pages/SelectPage/SelectPage";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -13,6 +14,9 @@ const App: React.FC = () => {
         </Route>
         <Route path="/join">
           <JoinPage />
+        </Route>
+        <Route path="/poker">
+          <SelectPage />
         </Route>
       </Switch>
     </div>
---
+++ src/pages/SelectPage/SelectPage.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import Page from "../../components/Page/Page";
+import Button from "../../components/Button/Button";
+import "./SelectPage.css";
+
+const SelectPage: React.FC = () => {
+  const [storyPoints, setStoryPoints] = useState([
+    { value: "1", selected: false },
+    { value: "2", selected: false },
+    { value: "3", selected: false },
+    { value: "5", selected: false },
+    { value: "8", selected: false },
+    { value: "13", selected: false },
+    { value: "21", selected: false },
+    { value: "?", selected: false },
+  ]);
+
+  const handleClick = (value: string) => {
+    const newStoryPoints = storyPoints.map((point) =>
+      point.value === value ? { ...point, selected: true } : { ...point, selected: false }
+    );
+    setStoryPoints(newStoryPoints);
+  };
+
+  const cards = storyPoints.map((point) => (
+    <div
+      className={`card ${point.selected ? "selected" : ""}`}
+      key={point.value}
+      onClick={() => handleClick(point.value)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(https://picsum.photos/seed/${
+            point.value !== "?" ? point.value : "34"
+          }${new Date().getDay()}/200)`,
+        }}
+      >
+        <div>{point.value}</div>
+      </div>
+    </div>
+  ));
+
+  const buttonEnabled = storyPoints.reduce((acc: boolean, curr: { value: string; selected: boolean }): boolean => {
+    return acc || curr.selected;
+  }, false);
+
+  return (
+    <Page className="SelectPage" color="yellow">
+      <header>
+        <h1>SELECT</h1>
+      </header>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div className="cards">{cards}</div>
+        <Button type="contained" label="SUBMIT" color="pink" disabled={!buttonEnabled} />
+      </div>
+    </Page>
+  );
+};
+
+export default SelectPage;
